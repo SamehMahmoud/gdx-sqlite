f6100fc677ba15d3b859a9b9105f77971a7d1fbb
SamehMahmoud <sameh.java@gmail.com>
2014-09-16 17:11:10 +0300

    Update SQLitePreparedStatement.java

--- gdx-sqlite/src/com/badlogic/gdx/sql/SQLitePreparedStatement.java
+++ gdx-sqlite/src/com/badlogic/gdx/sql/SQLitePreparedStatement.java
@@ -3,19 +3,19 @@ package com.badlogic.gdx.sql;
 
 public interface SQLitePreparedStatement {
 		
-	public void setInt(int pos , int value);
+	public void setInt(int pos , int value) throws SQLiteGdxException;
 	
-	public void setString(int pos , String value);
+	public void setString(int pos , String value) throws SQLiteGdxException;
 	
-	public void setDouble(int post , double value);
+	public void setDouble(int post , double value) throws SQLiteGdxException;
 	
-	public void setFloat(int pos , float value);
+	public void setFloat(int pos , float value) throws SQLiteGdxException;
 	
-	public void setBytes(int pos , byte[] bytes);
+	public void setBytes(int pos , byte[] bytes) throws SQLiteGdxException;
 	
-	public void setLong(int pos , long value);
+	public void setLong(int pos , long value) throws SQLiteGdxException;
 	
-	public boolean execute();
+	public boolean execute() throws SQLiteGdxException;
 
 }
 
